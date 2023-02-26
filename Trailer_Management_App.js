c17f4395f62166ac4d5170bf71426fb3832c61a9
Justin <94314648+xJstn1990x@users.noreply.github.com>
2023-02-25 23:59:40 -0500

    myapply

--- Trailer_Management_App.js
+++ Trailer_Management_App.js
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
+
+
+
 // Create an array of trailer objects
 let trailers = [
-    { id: 1, type: 'Dry Van', size: '53ft', status: 'Available', location: 'Warehouse 1' },
-    { id: 2, type: 'Reefer', size: '48ft', status: 'In Use', location: 'Customer A' },
-    { id: 3, type: 'Flatbed', size: '45ft', status: 'Scheduled for Maintenance', location: 'Maintenance Shop' }
+    { id: 9640, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9641, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9642, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9643, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9644, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9645, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9646, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9647, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9648, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9649, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9650, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9651, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9652, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9653, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9654, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9655, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9656, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9657, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9658, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9659, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9660, Type: 'Dry Van', Size: "53'", Location: 'Unknown'},
+    { id: 9661, Type: 'Dry Van', Size: "53'", Location: 'Unknown'}
   ];
   
   // Function to display all trailers in the inventory
@@ -10,7 +33,7 @@ let trailers = [
     console.log('Trailer Inventory:');
     console.log('------------------');
     trailers.forEach(trailer => {
-      console.log(`ID: ${trailer.id}, Type: ${trailer.type}, Size: ${trailer.size}, Status: ${trailer.status}, Location: ${trailer.location}`);
+      console.log(`ID: ${trailer.id}, Size: ${trailer.Size}, Status: ${trailer.status}, Location: ${trailer.Location}`);
     });
     console.log('');
   }
@@ -31,7 +54,7 @@ let trailers = [
     }
   }
   
-  // Example usage:
+
   displayTrailers(); // Display all trailers in the inventory
   updateTrailerStatus(2, 'Available'); // Update the status of trailer 2 to 'Available'
   displayTrailers(); // Display all trailers in the inventory again
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,20 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import react from 'react';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Home Page</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">This is a test for tab 1</IonTitle>
           </IonToolbar>
+          <IonButton>Test button</IonButton>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
